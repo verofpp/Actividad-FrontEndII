0c6be2f69f3b85b9f79f02ac2600e1778199ef0e
verofpp <147213778+verofpp@users.noreply.github.com>
2025-04-10 12:09:12 -0400

    Estructura FrontEnd

---
+++ FrontEnd/vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import flowbiteReact from "flowbite-react/plugin/vite";
+import tailwindcss from "@tailwindcss/vite";
+
+// https://vite.dev/config/
+export default defineConfig({
+  server: {
+    port: 3000,
+  },
+  plugins: [react(), flowbiteReact(), tailwindcss()],
+});
